8be4a5b7298a72372ec71ac8c179b805575cf71b
vithaln <vnnivargi1998@gmail.com>
2022-12-16 08:44:33 +0530

    added side bar and added JS to work

--- SmartContactManager/src/main/java/com/vithal/code/controller/UserController.java
+++ SmartContactManager/src/main/java/com/vithal/code/controller/UserController.java
@@ -1,16 +1,37 @@
 package com.vithal.code.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.vithal.code.entities.User;
+import com.vithal.code.repository.UserRepo;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	private UserRepo repo;
 	@GetMapping("/index")
-	public String dashBoard() {
+	public String dashBoard(Model model,Principal principal) {
 		
+		
+		//get the user by using userName(email)
+		
+		String userName = principal.getName();
+		System.out.println("USER ==> "+userName);
+		
+		User user = repo.getUserByUserName(userName);
+		System.out.println("USER DETAILS ARE "+user);
+		
+		//send data to dashboard
+		
+		model.addAttribute("user",user);
 		return "normal/user_dashboard";
 	}
 }
--- SmartContactManager/src/main/resources/static/js/script.js
+++ SmartContactManager/src/main/resources/static/js/script.js
@@ -1,1 +1,17 @@
 console.log("this is script file...")
+
+ toggleSidebar=()=>{
+if($(".sidebar").is(":visible")){
+//if true please close it
+
+$(".sidebar").css("display", "none");
+$(".content").css("margin-left","0%");
+}
+else{
+
+    //if false please show it
+    $(".sidebar").css("display","block");
+$(".content").css("margin-left","20%");
+
+}
+};
